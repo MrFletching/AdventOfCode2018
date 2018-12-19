d3004ad5b9c4a38a473340e509d0be3cecdb8366
Tom Fletcher <hello@tomfletcher.me>
2018-12-19 09:26:01 +0000

    Fix clearing registers bug

--- day19/19-1.c
+++ day19/19-1.c
@@ -67,7 +67,7 @@ void init_program(Program *p) {
     p->ip_reg = 0;
     
     for(int i = 0; i < REGISTER_COUNT; i++) {
-        p->registers[0] = 0;
+        p->registers[i] = 0;
     }
     
     p->instruction_count = 0;
